4225bd992decd2e4bbc765de32fd9a3b4229d2ce
Diniranga <dinirangapremanayake@gmail.com>
2023-09-01 18:47:38 +0530

    fixed some bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route,Navigate} from "react-router-dom";
 import Home from "./Home";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Create from "./Create";
@@ -11,6 +11,7 @@ function App() {
   return (
       <BrowserRouter>
         <Routes>
+            <Route path='/' element={<Navigate to="/student" />} />
           <Route path='/student' element={<Home/>} />
           <Route path='/create' element={<Create/>} />
             <Route path='/read/:id' element={<Read/>} />
--- frontend/src/Read.jsx
+++ frontend/src/Read.jsx
@@ -21,10 +21,10 @@ function Read() {
     return (
         <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
             <div className='w-50 bg-white rounded p-3'>
-                <h2>Student Details</h2>
-                <h2>{student[0]?.id}</h2>
-                <h2>{student[0]?.name}</h2>
-                <h2>{student[0]?.age}</h2>
+                <h2>Student Details</h2><hr/>
+                <h2> ID: {student[0]?.id}</h2>
+                <h2> NAME: {student[0]?.name}</h2>
+                <h2> AGE: {student[0]?.age}</h2><hr/>
                 <Link to='/student' className='btn btn-primary me-2'>Back</Link>
                 <Link to={`/edit/${student[0]?.id}`} className='btn btn-info'>Edit</Link>
             </div>
